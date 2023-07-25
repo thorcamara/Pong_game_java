0403a572fbd283eab6c5e81cc2f20429dfae0e6a
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-25 20:51:40 -0300

    Filled some GamePanel methods and Paddle method.

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class GamePanel extends JPanel implements Runnable {
 
     static final int GAME_WIDTH = 1000;
-    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (5/9));
+    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
     static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
     static final int BALL_DIAMETER = 20;
     static final int PADDLE_WIDTH = 25;
@@ -22,19 +22,30 @@ public class GamePanel extends JPanel implements Runnable {
     Ball ball;
     Score score;
 
-
     GamePanel() {
+        newPaddles();
+        newBall();
+        score = new Score(GAME_WIDTH, GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
 
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 
     public void newBall() {
 
     }
     public void newPaddles() {
-
+        paddle1 = new Paddle(0,(GAME_HEIGHT/2)-(PADDLE_HEIGTH/2),PADDLE_WIDTH,PADDLE_HEIGTH,1);
+        paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH,(GAME_HEIGHT/2)-(PADDLE_HEIGTH/2),PADDLE_WIDTH,PADDLE_HEIGTH,2);
     }
     public void paint(Graphics g) {
-
+        image = createImage(getWidth(), getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image,0,0,this);
     }
     public void draw(Graphics g) {
 
@@ -46,7 +57,22 @@ public class GamePanel extends JPanel implements Runnable {
 
     }
     public void run() {
-
+        //game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now -lastTime)/ns;
+            lastTime = now;
+            if(delta >= 1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+        }
     }
     public class AL extends KeyAdapter {
         public void keyPressed(KeyEvent e) {
