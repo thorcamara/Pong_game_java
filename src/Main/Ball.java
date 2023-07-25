80ec6eaef4a801b41ec7b96075451050e7d2390f
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-25 18:38:11 -0300

    Initial commit.

---
+++ src/Main/Ball.java
@@ -0,0 +1,9 @@
+package Main;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Ball {
+}
---
+++ src/Main/PongGame.java
@@ -0,0 +1,12 @@
+package Main;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class PongGame {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
