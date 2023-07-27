de61e8c01bed1b25f82284f31554bdd485f53cd2
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-26 21:30:44 -0300

    Added Score.

--- src/Main/Score.java
+++ src/Main/Score.java
@@ -21,5 +21,8 @@ public class Score extends Rectangle {
         g.setFont(new Font("Consolas",Font.PLAIN,60));
 
         g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT);
+
+        g.drawString(String.valueOf(player1/10)+String.valueOf(player1%10), (GAME_WIDTH/2)-85, 50);
+        g.drawString(String.valueOf(player2/10)+String.valueOf(player2%10), (GAME_WIDTH/2)+20, 50);
     }
 }
